00412c589e681538e74f462b46d7695389b84a9a
princediscord <51452946+princediscord@users.noreply.github.com>
2020-01-16 23:31:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "p";
-const developers = ("581936755532365826")
+const developers = ("620689121211449351")
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith('p' + 'pl')) {
+  if (message.content.startsWith('pl')) {
     client.user.setGame(argresult);
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-     if (message.content === ('p' + "خروج")) {
+     if (message.content === ("خروج")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith('p' + 'wt')) {
+  if (message.content.startsWith('wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('p' + 'ls')) {
+  if (message.content.startsWith('ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**✅   ${argresult}**`)
   } else 
-  if (message.content.startsWith('p' + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
+  if (message.content.startsWith('st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/groza");
       message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith(' p' + 'name')) {
+  if (message.content.startsWith('name')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith('p' + 'pic')) {
+if (message.content.startsWith('pic')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
   
